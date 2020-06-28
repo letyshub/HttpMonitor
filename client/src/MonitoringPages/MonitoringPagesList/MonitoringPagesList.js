d94c091caf1bee4f8ad9426292bfa3dced993748
letyshub <letys@int.pl>
2020-06-28 16:21:02 +0200

    initial commit

---
+++ client/src/MonitoringPages/MonitoringPagesList/MonitoringPagesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './MonitoringPagesList.css';
+
+const monitoringPagesList = ( props ) => {
+    return (
+        <div>
+        <a class="waves-effect waves-light btn-small" href="#"><i class="material-icons left">add</i >create</a>
+        <p>sss</p>
+        </div>
+    );
+};
+
+export default monitoringPagesList;
